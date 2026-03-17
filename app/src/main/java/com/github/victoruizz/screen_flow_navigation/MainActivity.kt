2155f1ce4241b4e8eaf0b7a29dc083821a655fc4
Victor Ruiz <ruizvictor077@outlook.com>
2026-03-17 08:45:27 -0300

    Adiciona chamada da LoginScreen na MainActivity como tela inicial do app

--- app/src/main/java/com/github/victoruizz/screen_flow_navigation/MainActivity.kt
+++ app/src/main/java/com/github/victoruizz/screen_flow_navigation/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.github.victoruizz.screen_flow_navigation.screens.LoginScreen
 import com.github.victoruizz.screen_flow_navigation.ui.theme.ScreenflownavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ScreenflownavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
